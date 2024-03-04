ccd3f6515d6fa6c53bb86f8cb0cd595530c1525a
Krigo-Liu <liuyuhailiuyuhao@gmail.com>
2024-03-04 18:18:00 +0800

    Change config

--- Krigo_blog/src/.vuepress/config.ts
+++ Krigo_blog/src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/Krigo_blog/",
+  base: "/blog/",
 
   lang: "zh-CN",
   title: "Krigo's 博客",
