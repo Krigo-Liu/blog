47c2895a4a2434a4aa6d71e19ec1733066b59f44
Krigo-Liu <liuyuhailiuyuhao@gmail.com>
2024-03-12 10:18:34 +0800

    Change the setting.

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,9 +2,8 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  "/demo/",
   {
-    text: "博文",
+    text: "Note",
     icon: "pen-to-square",
     prefix: "/posts/",
     children: [
--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -3,20 +3,13 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar({
   "/": [
     "",
+    "intro",
     {
-      text: "如何使用",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "文章",
+      text: "Note",
       icon: "book",
       prefix: "posts/",
       children: "structure",
     },
-    "intro",
     {
       text: "幻灯片",
       icon: "person-chalkboard",
